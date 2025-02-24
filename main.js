239883070c8c03a077da8d2dedcefbbb88db5984
Tinneea <tindrajedensjo@gmail.com>
2025-02-24 14:53:53 +0100

    24 feb

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+const overlayElement = document.getElementById("overlay");
+const shopByElement = document.getElementById("shop-by");
+
+shopByElement.addEventListener("mouseover", function (e) {
+    overlayElement.style.display = "block";
+});
+shopByElement.addEventListener("mouseout", function (e) {
+    overlayElement.style.display = "none";
+});
+overlayElement.addEventListener("mouseover", function (e) {
+    overlayElement.style.display = "block";
+});
+overlayElement.addEventListener("mouseout", function (e) {
+    overlayElement.style.display = "none";
+}); 
