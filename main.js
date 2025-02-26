d80a024aba8a36753bc9add58132cb58c10bb294
cure23 <cure23xv@student.ju.se>
2025-02-26 11:29:39 +0100

    26.02.2025

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
-
-
 const overlayElement = document.getElementById("overlay");
 const shopByElement = document.getElementById("shop-by");
+const overLinkElement = document.getElementsByClassName("over-link");
 
 shopByElement.addEventListener("mouseover", function (e) {
-    overlayElement.style.display = "block";
+  overlayElement.style.display = "block";
 });
 shopByElement.addEventListener("mouseout", function (e) {
-    overlayElement.style.display = "none";
+  overlayElement.style.display = "none";
 });
 overlayElement.addEventListener("mouseover", function (e) {
-    overlayElement.style.display = "block";
+  overlayElement.style.display = "block";
 });
 overlayElement.addEventListener("mouseout", function (e) {
-    overlayElement.style.display = "none";
-}); 
+  overlayElement.style.display = "none";
+});
+
+/* overLinkElement.addEventListener("mouseover", function (e) {
+  overLinkElement.style.color = "#FFFF00";
+}); */
