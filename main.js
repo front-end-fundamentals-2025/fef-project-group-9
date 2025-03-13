aa940fb4cbb2d0711922a86d90135fef0fff241b
cure23 <cure23xv@student.ju.se>
2025-03-13 19:36:41 +0100

    sparkles, cart and dot

--- main.js
+++ main.js
@@ -18,138 +18,126 @@ overlayElement.addEventListener("mouseout", function (e) {
   overlayElement.style.display = "none";
 });
 
-// Option 1:
+function showCartCount(cart) {
+  if (cartCountElement !== null) {
+    if (cart.length > 0) {
+      cartCountElement.style.display = "block";
+      cartCountElement.innerText = cart.length;
+    } else {
+      cartCountElement.style.display = "none";
+    }
+  }
+}
 
+// ADD TO CART
 const cartButton = document.getElementById("d-button");
+const cartCountElement = document.getElementById("cart-count");
 
 if (cartButton !== null) {
-  // +++++ THIS IF STATEMENT IS NEW +++++
-
-  const cartName = document.getElementById("cart-name");
+  const cartItem = {
+    name: document.getElementById("d-name").innerText,
+    price: document.getElementById("d-price").innerText,
+    img: document.getElementById("d-img").src,
+  };
 
   let cart;
 
   //  !!!  THIS WORKS  !!! - change back button class to id
-
+  if (localStorage.cart) {
+    cart = JSON.parse(localStorage.cart);
+    showCartCount(cart);
+  } else {
+    // if not, create a new cart
+    cart = [];
+  }
   cartButton.addEventListener("click", function (e) {
     // check if we already have a cart
 
-    if (localStorage.cart) {
-      cart = JSON.parse(localStorage.cart);
-    } else {
-      // if not, create a new cart
-
-      cart = [];
-    }
-
-    cart.push(cartName.innerText);
-
+    cart.push(cartItem);
     localStorage.cart = JSON.stringify(cart);
+    showCartCount(cart);
   });
-} // +++++ NEW IF STATEMENT END +++++
-
-// Option 2: USE CLASSES AS ARRAYS - one 'for-loop' = no 100 != ids and no copy paste
-
-// nb - change button 'id' in html to 'class'
-
-/* const cartButtons = document.getElementsByClassName("d-button");
-
-const headerElements = document.getElementsByClassName("d-header");
-
-let cart;
-
-
-
-for (let i = 0; i < cartButtons.length; i++) {
-
-  const cartButton = cartButtons[i]; // get prod-button
-
-  const headerElement = headerElements[i]; // get prod-header
-
-  cartButton.addEventListener("click", function (e) {
-
-    // check if we already have a cart
-
-    if (localStorage.cart) {
-
-      cart = JSON.parse(localStorage.cart);
-
-    } else {
-
-      // if not, create a new cart
-
-      cart = [];
-
-    }
-
-    cart.push(headerElement.innerText);
-
-    localStorage.cart = JSON.stringify(cart);
-
-  });
-
-} */
-
-// +++++ NEW STUFF +++++
+}
 
 // DISPLAY CART LOGIC
 
-const cartItems = document.getElementById("cart-items");
+const cartUlElement = document.getElementById("cart-items");
 
 if (localStorage.cart) {
-  let cartTwo = JSON.parse(localStorage.cart);
-
-  if (cartTwo.length === 0) {
+  let cart = JSON.parse(localStorage.cart);
+  showCartCount(cart);
+  if (cart.length === 0) {
     const emptyElement = document.createElement("li");
     emptyElement.classList.add("new-in-blurb");
     emptyElement.innerText = "Your cart is empty.";
-    cartItems.appendChild(emptyElement);
+    cartUlElement.appendChild(emptyElement);
+    showCartCount(cart);
   } else {
-    for (let i = 0; i < cartTwo.length; i++) {
-      let item = cartTwo[i];
-      const nameElement = document.createElement("p");
-      nameElement.classList.add("cart-name");
-      nameElement.innerText = item;
+    for (let i = 0; i < cart.length; i++) {
+      let item = cart[i];
+      const imgElement = document.createElement("img");
+      imgElement.classList.add("cart-img");
+      imgElement.src = item.img;
+
+      const textElement = document.createElement("p");
+      textElement.classList.add("cart-text");
+      textElement.innerHTML = item.name;
+
+      const priceElement = document.createElement("p");
+      priceElement.classList.add("cart-price");
+      priceElement.innerHTML = item.price;
+
       const buttonElement = document.createElement("button");
       buttonElement.innerText = "remove";
       buttonElement.classList.add("cart-button");
+
       const liElement = document.createElement("li");
-      liElement.appendChild(nameElement);
+      liElement.appendChild(imgElement);
+      liElement.appendChild(textElement);
+      liElement.appendChild(priceElement);
       liElement.appendChild(buttonElement);
       liElement.classList.add("new-in-blurb");
       liElement.classList.add("cart-list");
-      cartItems.appendChild(liElement);
+
+      cartUlElement.appendChild(liElement);
+
+      // romove items from cart
+      buttonElement.addEventListener("click", function () {
+        liElement.remove();
+
+        let updatedCart = [];
+        const remainingCartElements =
+          document.getElementsByClassName("cart-list");
+
+        //Update cart in local storage
+        for (let j = 0; j < remainingCartElements.length; j++) {
+          const cartItemElement = remainingCartElements[j];
+
+          const updatedCartItem = {
+            name: cartItemElement.querySelector(".cart-text").innerText,
+            price: cartItemElement.querySelector(".cart-price").innerText,
+            img: cartItemElement.querySelector(".cart-img").src,
+          };
+
+          updatedCart.push(updatedCartItem);
+        }
+
+        if (updatedCart.length === 0) {
+          const emptyElement = document.createElement("li");
+          emptyElement.classList.add("new-in-blurb");
+          emptyElement.innerText = "Your cart is empty.";
+          cartUlElement.appendChild(emptyElement);
+        }
+
+        localStorage.cart = JSON.stringify(updatedCart);
+        showCartCount(updatedCart);
+      });
     }
   }
 } else {
   const emptyElement = document.createElement("li");
   emptyElement.classList.add("new-in-blurb");
   emptyElement.innerText = "Your cart is empty.";
-  cartItems.appendChild(emptyElement);
-}
-
-const cartNameElements = document.getElementsByClassName("cart-name");
-const cartButtonElements = document.getElementsByClassName("cart-button");
-
-for (let i = 0; i < cartNameElements.length; i++) {
-  const cartNameElement = cartNameElements[i];
-  const cartButtonElement = cartButtonElements[i];
-  cartButtonElement.addEventListener("click", function (e) {
-    cartNameElement.parentElement.remove();
-
-    const updatedCart = [];
-
-    const remainingCartElements = document.getElementsByClassName("cart-name");
-
-    for (let j = 0; j < remainingCartElements.length; j++) {
-      updatedCart.push(remainingCartElements[j].innerText);
-    }
-    if (updatedCart.length === 0) {
-      const emptyElement = document.createElement("li");
-      emptyElement.classList.add("new-in-blurb");
-      emptyElement.innerText = "Your cart is empty.";
-      cartItems.appendChild(emptyElement);
-    }
-    localStorage.cart = JSON.stringify(updatedCart);
-  });
+  cartUlElement.appendChild(emptyElement);
+  showCartCount(updatedCart);
 }
